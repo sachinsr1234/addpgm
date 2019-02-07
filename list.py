3191e46f9d28d9da97e4a90ccbba986ba70ce445
sachinsr1234 <sachinsr756412@gmail.com>
2019-02-07 09:58:30 +0530

    list

--- list.py
+++ list.py
@@ -1,2 +1,8 @@
-subjects=["physics","chemistry",123]
+subjects=["physics","chemistry","maths","english",123]
+print(subjects)
+print(subjects[0])
+print(subjects[0:2])
+subjects[0]="hindi"
+print(subjects)
+del subjects[1]
 print(subjects)
